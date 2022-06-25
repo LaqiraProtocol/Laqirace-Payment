f36d4d7fe8cebb76aeef0e73476a078dca515114
lightY <lighty8214@gmail.com>
2022-06-25 22:45:50 +0430

    deposit function was renamed to depositToken

--- contracts/LaqiracePayment.sol
+++ contracts/LaqiracePayment.sol
@@ -9,7 +9,8 @@ contract LaqiracePayment is Ownable {
     mapping(address => bool) private quoteToken;
 
     address private paymentReceiver;
-    function deposit(address _quoteToken, address _player, uint256 _amount) public {
+
+    function depositToken(address _quoteToken, address _player, uint256 _amount) public {
         require(quoteToken[_quoteToken], 'Payment method is not allowed');
         TransferHelper.safeTransferFrom(_quoteToken, _msgSender(), paymentReceiver, _amount);
         emit Deposit(_player, _quoteToken, _amount);
