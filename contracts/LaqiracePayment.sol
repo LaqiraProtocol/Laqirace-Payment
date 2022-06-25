fe9bb8e1a32eadd97fdcd3e86c816af9d49f9d70
lightY <lighty8214@gmail.com>
2022-06-25 22:37:36 +0430

    Ownable contract was imported

--- contracts/LaqiracePayment.sol
+++ contracts/LaqiracePayment.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/access/Ownable.sol';
+
 import './TransferHelper.sol';
 
-contract LaqiracePayment {
+contract LaqiracePayment is Ownable {
     
 }
