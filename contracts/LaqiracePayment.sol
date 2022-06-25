aef4c0f2d1454c9d822029f4bd46e15d76278c63
lightY <lighty8214@gmail.com>
2022-06-25 23:02:17 +0430

    functions of withdraw request were developed

--- contracts/LaqiracePayment.sol
+++ contracts/LaqiracePayment.sol
@@ -13,6 +13,8 @@ contract LaqiracePayment is Ownable {
 
     event DepositToken(address player, address quoteToken, uint256 amount);
     event DepositNativeCurrency(address player, string curreny, uint256 amount);
+    event WithdrawTokenRequest(address player, address quoteToken, uint256 amount);
+    event WithdrawNativeRequest(address player, string currency, uint256 amount);
 
     function depositToken(address _quoteToken, address _player, uint256 _amount) public {
         require(quoteToken[_quoteToken], 'Payment method is not allowed');
@@ -23,5 +25,13 @@ contract LaqiracePayment is Ownable {
     function depositNativeCurrency(address _player) public payable {
         require(nativeCurrencyPermit, 'Payment method not allowed');
         emit DepositNativeCurrency(_player, 'BNB', msg.value);
+    }
+
+    function withdrawTokenRequest(address _quoteToken, uint256 _amount) public {
+        emit WithdrawTokenRequest(_msgSender(), _quoteToken, _amount);
+    }
+
+    function withdrawNativeRequest(uint256 _amount) public {
+        emit WithdrawNativeRequest(_msgSender(), 'BNB', _amount);
     }
 }
