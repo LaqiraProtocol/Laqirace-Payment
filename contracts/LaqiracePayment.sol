99a2776d7ae63a6b3beb6c27e9c96dad1f2e6a93
lightY <lighty8214@gmail.com>
2022-06-25 22:55:45 +0430

    depositNativeCurrency function and deposit events were developed

--- contracts/LaqiracePayment.sol
+++ contracts/LaqiracePayment.sol
@@ -9,10 +9,19 @@ contract LaqiracePayment is Ownable {
     mapping(address => bool) private quoteToken;
 
     address private paymentReceiver;
+    bool private nativeCurrencyPermit;
+
+    event DepositToken(address player, address quoteToken, uint256 amount);
+    event DepositNativeCurrency(address player, string curreny, uint256 amount);
 
     function depositToken(address _quoteToken, address _player, uint256 _amount) public {
         require(quoteToken[_quoteToken], 'Payment method is not allowed');
         TransferHelper.safeTransferFrom(_quoteToken, _msgSender(), paymentReceiver, _amount);
-        emit Deposit(_player, _quoteToken, _amount);
+        emit DepositToken(_player, _quoteToken, _amount);
+    }
+
+    function depositNativeCurrency(address _player) public payable {
+        require(nativeCurrencyPermit, 'Payment method not allowed');
+        emit DepositNativeCurrency(_player, 'BNB', msg.value);
     }
 }
